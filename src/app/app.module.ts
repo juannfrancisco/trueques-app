20924f25e2eeec079989695c189f55798550a631
Juan Francisco Maldonado Leon <juan.maldonado.leon@gmail.com>
2021-05-31 16:11:49 -0400

    02 - Crear componente principal llamado “Landing” y asociar una ruta.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing/landing.component';
+import { NavbarComponent } from './components/landing/navbar/navbar.component';
+import { FooterComponent } from './components/landing/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
